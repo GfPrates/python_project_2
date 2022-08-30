34477cf8bed9f9fd9a5d8edf6eff9640bd1fb6b6
Gabriela <gabrielafprates@gmail.com>
2022-08-29 21:41:11 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#importando a biblioteca
+from tkinter import *
+from tkinter import ttk
+
+#cores
+cor1= '#33ff33' #verde
+cor2= '#ffffff' #branca
+cor3= '#141452' #azul
+cor4= '#b8b894' #cinza
+cor5= '#ff00aa' #rosa
+
+#chamando a janela para tela
+janela = Tk()
+janela.title('Calculadora')
+janela.geometry('225x308')
+
+
+janela.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
